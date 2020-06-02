364c31cf5058191ae59bdf9700163388166d98a8
TestingRavisankar <krsk1499@gmail.com>
2020-06-02 20:25:04 +0530

    Ravi Test

---
+++ src/main/java/TestMe123/TestMe123/App.java
@@ -0,0 +1,13 @@
+package TestMe123.TestMe123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TestMe123/TestMe123/AppTest.java
@@ -0,0 +1,20 @@
+package TestMe123.TestMe123;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
